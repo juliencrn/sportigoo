e160b4de753ef51e15c3073c2634f235391470be
Gitium <gitium@presslabs.com>
2020-03-17 10:49:09 +0100

    Merged changes from sportigoo.fr on 03.17.2020

--- pwa-amp-sw.js
+++ pwa-amp-sw.js
@@ -5,15 +5,15 @@ const BASE_CACHE_FILES = [
 ];
 
 const OFFLINE_CACHE_FILES = [
-     'https://sportigoo.fr/sportigoo-qui-sommes-nous/?amp=1',
+     'https://sportigoo.fr/les-10-meilleures-activites-pour-un-team-building-a-paris/?amp=1',
 ];
 
 const NOT_FOUND_CACHE_FILES = [
-    'https://sportigoo.fr/sportigoo-qui-sommes-nous/?amp=1',
+    'https://sportigoo.fr/les-10-meilleures-activites-pour-un-team-building-a-paris/?amp=1',
 ];
 
-const OFFLINE_PAGE = 'https://sportigoo.fr/sportigoo-qui-sommes-nous/?amp=1';
-const NOT_FOUND_PAGE = 'https://sportigoo.fr/sportigoo-qui-sommes-nous/?amp=1';
+const OFFLINE_PAGE = 'https://sportigoo.fr/les-10-meilleures-activites-pour-un-team-building-a-paris/?amp=1';
+const NOT_FOUND_PAGE = 'https://sportigoo.fr/les-10-meilleures-activites-pour-un-team-building-a-paris/?amp=1';
 
 const CACHE_VERSIONS = {
     content: 'content-v' + CACHE_VERSION,
