39d1e645b6873a8acde3979f9918cfd60caba881
Mounir Bahrir <moonbahrir@gmail.com>
2020-04-13 21:48:58 +0200

    modified file `pwa-sw.js` (after activation of `Autoptimize` version 2.5.1)

--- pwa-sw.js
+++ pwa-sw.js
@@ -17,15 +17,15 @@ const BASE_CACHE_FILES = [
 ];
 
 const OFFLINE_CACHE_FILES = [
-     'https://sportigoo.fr/activite-team-building-paris/',
+     'https://sportigoo.fr/',
 ];
 
 const NOT_FOUND_CACHE_FILES = [
-    'https://sportigoo.fr/activite-team-building-paris/',
+    'https://sportigoo.fr/',
 ];
 
-const OFFLINE_PAGE = 'https://sportigoo.fr/activite-team-building-paris/';
-const NOT_FOUND_PAGE = 'https://sportigoo.fr/activite-team-building-paris/';
+const OFFLINE_PAGE = 'https://sportigoo.fr/';
+const NOT_FOUND_PAGE = 'https://sportigoo.fr/';
 
 const CACHE_VERSIONS = {
     content: 'content-v' + CACHE_VERSION,
